2667181819130976437f62cf8dc03ec54a78a6d9
Ashwin Shah <on.mony@gmail.com>
2021-04-10 21:59:02 +0530

    Pushing the ui with components

---
+++ Lesson3/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+function App() {
+  console.log("Inside function app");
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
